e16e453868fce16e1623b7e0dd128f8942f64395
SJeongheon <fight0105@gmail.com>
2024-12-20 11:41:13 +0900

    initialization

---
+++ src/main/java/com/emergency/emergency_detection/EmergencyDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.emergency.emergency_detection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyDetectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emergency/emergency_detection/EmergencyDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emergency.emergency_detection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
